444c7d48889c448832949e5598efe1eb1bd4d687
Anna Kimtis <Anna@Annas-MBP.verizon.net>
2017-02-03 19:13:31 -0500

    spotify and do what it says functions are working

--- liri.js
+++ liri.js
@@ -16,12 +16,26 @@ var fs = require('fs');
  
 file.readFile === fs.readFile // true 
 
-function spotifySeach(song) {
-    var song = process.argv[3];
+
+
+var spotifySearch = function() {
+  console.log("spotifySearching ");
+    var song = "";
+    for (var i=3; i<nodeArgs.length; i++ ){
+      console.log(nodeArgs[i]);
+      if (i>3){
+        song = song +"+"+ nodeArgs[i]
+      }else{
+        song += nodeArgs[i]
+      };
+    };
+      console.log("song is "+song);
+    // var song = process.argv[3];
     if(!song){
       song = "The Sign";
     }
     params = song;
+    
     spotify.search({ type: "track", query: params }, function(err, data) {
       if(!err){
         var songInfo = data.tracks.items;
@@ -32,7 +46,7 @@ function spotifySeach(song) {
             "Song: " + songInfo[i].name + "\r\n" +
             "Preview Url: " + songInfo[i].preview_url + "\r\n" + 
             "Album the song is from: " + songInfo[i].album.name + "\r\n";
-            console.log(spotifyResults);
+            console.log("spotifyResults " +spotifyResults);
           }
         }
       } else {
@@ -40,7 +54,8 @@ function spotifySeach(song) {
         return;
       }
     });
-  };
+};
+
 
 
 var twitterDisplay = function(){
@@ -76,34 +91,36 @@ var movieSearch = function(){
   var movie ="";
 
   for (var i=3; i<nodeArgs.length; i++ ){
-    console.log(nodeArgs[i]);
+    console.log("movieArg "+nodeArgs[i]);
     if (i>3){
       movie = movie +"+"+ nodeArgs[i]
     }else{
       movie += nodeArgs[i]
     };
-    console.log(movie);
+    console.log("movie "+ movie);
   }
   
   request("http://www.omdbapi.com/?t="+movie+"=&plot=short&r=json", function(error, response, body) {
+    if (error){
+      console.log(error);
+    }
+    if (!error && response.statusCode === 200) {
 
-  if (!error && response.statusCode === 200) {
 
+      console.log("The movie's title is: " + JSON.parse(body).Title);
+      console.log("The Year the movie was released is: " + JSON.parse(body).Year);
+      console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+      console.log("The movie was produced in: " + JSON.parse(body).Country);
+      console.log("The movie's language is: " + JSON.parse(body).Language);
+      console.log("The movie's plot is: " + JSON.parse(body).Plot);
+      console.log("The movie's actors are: " + JSON.parse(body).Actors);
+      console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+      console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
 
-    console.log("The movie's title is: " + JSON.parse(body).Title);
-    console.log("The Year the movie was released is: " + JSON.parse(body).Year);
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-    console.log("The movie was produced in: " + JSON.parse(body).Country);
-    console.log("The movie's language is: " + JSON.parse(body).Language);
-    console.log("The movie's plot is: " + JSON.parse(body).Plot);
-    console.log("The movie's actors are: " + JSON.parse(body).Actors);
-    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-    console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
+  //need to console log Mr. Nobody if person doesn't enter a movie
 
-//need to console log Mr. Nobody if person doesn't enter a movie
-
-  }
-});
+    }
+  });
 }
 
 var doWhatItSays = function(){
@@ -142,5 +159,4 @@ switch(nodeArgs[2]) {
     default:
         console.log("none")
 }
-
 
